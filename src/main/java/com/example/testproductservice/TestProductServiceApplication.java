fc8acf314d976a64442cd760ffaf365e4ded5b4d
Tran Thanh <tranthanhvt0982@gmail.com>
2026-02-16 21:39:11 +0700

    first commit

---
+++ src/main/java/com/example/testproductservice/TestProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProductServiceApplication.class, args);
+    }
+
+}
